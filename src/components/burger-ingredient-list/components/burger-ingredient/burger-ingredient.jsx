e64aa3575b7dc1169c03338744798f5f53700a8a
alexyasinsky <alex_yasinsky@mail.ru>
2023-11-26 17:44:52 +0300

    escapeHandler in Modal added

--- src/components/burger-ingredient-list/components/burger-ingredient/burger-ingredient.jsx
+++ src/components/burger-ingredient-list/components/burger-ingredient/burger-ingredient.jsx
@@ -16,6 +16,8 @@ export default function BurgerIngredient({ ingredient }) {
     setVisibilityOfIngredientDetails(!visibilityOfIngredientDetails);
   }
 
+
+
   return (
     <>
       <figure
--- src/components/modal/modal.jsx
+++ src/components/modal/modal.jsx
@@ -3,9 +3,20 @@ import { createPortal } from 'react-dom';
 import styles from './modal.module.scss';
 import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 
 export default function Modal({ children, title, onClose }) {
   const modalRoot = document.getElementById('react-modals');
+
+  function escapeHandler(event) {
+    return event.key === 'Escape' && onClose();
+  }
+  useEffect(()=> {
+    document.addEventListener('keydown', escapeHandler);
+    return (()=>{
+      document.removeEventListener('keydown', escapeHandler);
+    })
+  })
 
   return createPortal(
    <div className={styles.wrapper}>
