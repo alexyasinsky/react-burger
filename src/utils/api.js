706c2205f152ecc041108d9540e25479e2cc7d61
alexyasinsky <alex_yasinsky@mail.ru>
2024-01-04 18:35:29 +0300

    api.js beautified

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,15 @@
 
 export const url = 'https://norma.nomoreparties.space/api';
-
-export async function makeRequest(url, options = {}) {
-  const response = await fetch(url, options);
-  if (response.ok) {
-    return response.json();
-  }
-  return Promise.reject(`Ошибка ${response.status}`);
-}
-
 const checkResponse = (res) => {
   return res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
 };
+export async function makeRequest(url, options = {}) {
+  const response = await fetch(url, options);
+  return checkResponse(response);
+}
 
 export const refreshToken = async (url) => {
-  const res = await fetch(`${url}/auth/token`, {
+  const response = await fetch(`${url}/auth/token`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json;charset=utf-8",
@@ -23,12 +18,12 @@ export const refreshToken = async (url) => {
       token: localStorage.getItem("refreshToken"),
     }),
   });
-  return checkResponse(res);
+  return checkResponse(response);
 };
 export const makeRequestWithRefreshToken = async (url, options) => {
   try {
-    const res = await fetch(url, options);
-    return await checkResponse(res);
+    const response = await fetch(url, options);
+    return await checkResponse(response);
   } catch (err) {
     if (err.message === "jwt expired") {
       const refreshData = await refreshToken(); //обновляем токен
@@ -38,8 +33,8 @@ export const makeRequestWithRefreshToken = async (url, options) => {
       localStorage.setItem("refreshToken", refreshData.refreshToken);
       localStorage.setItem("accessToken", refreshData.accessToken);
       options.headers.authorization = refreshData.accessToken;
-      const res = await fetch(url, options); //повторяем запрос
-      return await checkResponse(res);
+      const response = await fetch(url, options); //повторяем запрос
+      return await checkResponse(response);
     } else {
       return Promise.reject(err);
     }
