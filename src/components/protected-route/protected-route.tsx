0583eadc377d50ffe066b82a8ddf383578e3b5a4
alexyasinsky <alex_yasinsky@mail.ru>
2024-01-24 17:49:26 +0300

    pacman preloader added

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -2,46 +2,53 @@ import {useSelector} from "react-redux";
 import {Navigate, useLocation} from "react-router-dom";
 import {selectIsAuthChecked, selectUser} from "../../services/store/user/reducers";
 import {JSX, ReactElement} from "react";
+import {PacmanLoader} from "react-spinners";
 
 type TProtectedRouteProps = {
-  onlyUnAuth?: boolean,
-  component: ReactElement
+    onlyUnAuth?: boolean,
+    component: ReactElement
 }
 
 type TOnlyUnAuthProps = Omit<TProtectedRouteProps, 'onlyUnAuth'>;
 
-const ProtectedRoute = ({onlyUnAuth = false, component}: TProtectedRouteProps) : JSX.Element => {
-  // isAuthChecked это флаг, показывающий, что проверка токена произведена.
-  // при этом результат этой проверки не имеет значения, важно только,
-  // что сам факт проверки имел место.
-  const isAuthChecked = useSelector(selectIsAuthChecked);
-  const user = useSelector(selectUser);
-  const location = useLocation();
+const ProtectedRoute = ({onlyUnAuth = false, component}: TProtectedRouteProps): JSX.Element => {
+    // isAuthChecked это флаг, показывающий, что проверка токена произведена.
+    // при этом результат этой проверки не имеет значения, важно только,
+    // что сам факт проверки имел место.
+    const isAuthChecked = useSelector(selectIsAuthChecked);
+    const user = useSelector(selectUser);
+    const location = useLocation();
 
-  if (!isAuthChecked) {
-    // Запрос еще выполняется
-    // Выводим прелоадер в ПР
-    // Здесь возвращается просто null для экономии времени
-    return (<></>);
-  }
+    if (!isAuthChecked) {
+        // Запрос еще выполняется
+        // Выводим прелоадер в ПР
+        return (
+            <PacmanLoader
+                loading={true}
+                color="#F2F2F3"
+                size={100}
+                aria-label="Loading Spinner"
+            />
+        );
+    }
 
-  if (onlyUnAuth && user) {
-    // Пользователь авторизован, но роут предназначен для неавторизованного пользователя.
-    // Делаем редирект на главную страницу или на тот адрес, что записан в location.state.from
-    const {from} = location.state || {from: {pathname: "/"}};
-    return (<Navigate to={from}/>);
-  }
+    if (onlyUnAuth && user) {
+        // Пользователь авторизован, но роут предназначен для неавторизованного пользователя.
+        // Делаем редирект на главную страницу или на тот адрес, что записан в location.state.from
+        const {from} = location.state || {from: {pathname: "/"}};
+        return (<Navigate to={from}/>);
+    }
 
-  if (!onlyUnAuth && !user) {
-    return (<Navigate to="/login" state={{from: location}}/>);
-  }
+    if (!onlyUnAuth && !user) {
+        return (<Navigate to="/login" state={{from: location}}/>);
+    }
 
 
-  // !onlyUnAuth && user Пользователь авторизован и роут для авторизованного пользователя
-  return component;
+    // !onlyUnAuth && user Пользователь авторизован и роут для авторизованного пользователя
+    return component;
 };
 
 export const OnlyAuth = ProtectedRoute;
 export const OnlyUnAuth = ({component}: TOnlyUnAuthProps) => (
-  (<ProtectedRoute onlyUnAuth={true} component={component}/>)
+    (<ProtectedRoute onlyUnAuth={true} component={component}/>)
 );
